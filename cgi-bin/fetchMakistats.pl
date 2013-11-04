892b335a8852fe3fbdc79fcf1724684ceda6cc5c
andrbmgi <andreas.raede@gmail.com>
2013-11-04 23:00:37 +0100

    clean up

--- cgi-bin/fetchMakistats.pl
+++ cgi-bin/fetchMakistats.pl
@@ -1,24 +1,5 @@
 #!/usr/bin/perl -w
-#exec('ulimit -n 2048; vendor_process');
-#use strict;
-no warnings;
-use CGI;
-use lib qw(/var/www/web85/html/makiscrape/cgipan/mylib/);
-use YAML;
-use lib qw(/var/www/web85/html/makiscrape/cgipan/build/Web-Scraper-0.32/lib/);
-use Web::Scraper;
-use LWP::UserAgent;
-use HTTP::Cookies;
-use lib qw(/var/www/web85/html/makiscrape/cgipan/Crypt-SSLeay-0.58/lib/);
-use Crypt::SSLeay;
-use LWP::Protocol::https;
-use Data::Dumper;
-use String::Util 'trim';
-use DateTime;
-use JSON;
 
-
-#open (MYFILE, '</var/www/web85/html/makiscrape/makiscrape.json') or die $!;
 open (MYFILE, '<makiscrape.json') or die $!;
 
 print "Access-Control-Allow-Origin: *\n";
@@ -29,5 +10,3 @@ while (<MYFILE>) {
 }
 
 close (MYFILE);
-
-
